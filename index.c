56b220ec470cc4ab3eea8cd92b76876cb0e35ec2
DivyamSingh <64833579+DivyamSingh18@users.noreply.github.com>
2020-09-29 18:59:44 +0530

    Create index.c

---
+++ index.c
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+
+void disp(void);
+
+int main()
+{
+    disp();
+    return 0;
+}
+
+void disp(void)
+{
+   cout<< "This function works fine :-)";
+}
